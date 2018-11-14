a5b7b24c9b1ff072869c296b2e12fa0c513a5b36
孙柳 <liu.sun@hand-china.com>
2018-11-14 16:03:57 +0800

    领取优惠券页面登录态判断

--- pages/coupon/couponReceive.js
+++ pages/coupon/couponReceive.js
@@ -1,18 +1,35 @@
 // pages/coupon/couponReceive.js
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    userInfo: ""
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo
+      })
+    }
+  },
 
+  getUserInfo: function (e) {
+    console.log(e.currentTarget.dataset)
+    let gotoPage = e.currentTarget.dataset.page
+    let userInfo = {};
+    if (e.detail.userInfo) {
+      userInfo = e.detail.userInfo;
+      wx.navigateTo({
+        url: '../login/login?userInfo=' + JSON.stringify(userInfo),
+      })
+    }
   },
 
   receive: function(){
@@ -44,7 +61,11 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo
+      })
+    }
   },
 
   /**
